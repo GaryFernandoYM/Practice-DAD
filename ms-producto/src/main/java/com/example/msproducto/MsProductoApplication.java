10b2a6adb46e7eb3e326066a0e2999d4f6c8dea7
GaryFernandoYM <miraclevann224@gmail.com>
2023-09-15 19:19:00 -0500

    ms-producto

--- ms-producto/src/main/java/com/example/msproducto/MsProductoApplication.java
+++ ms-producto/src/main/java/com/example/msproducto/MsProductoApplication.java
@@ -1,7 +1,10 @@
 package com.example.msproducto;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsProductoApplication {
@@ -10,4 +13,15 @@ public class MsProductoApplication {
 		SpringApplication.run(MsProductoApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIO PRODUCTO")
+				.version("0.0.1")
+				.description("servicios web Prpducto")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+
+		);
+	}
 }
